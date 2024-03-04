6a3769317f52e84b182699d4d6f173a6f0c968f4
prakharv07 <97461633+prakharv07@users.noreply.github.com>
2024-03-04 13:14:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,7 @@ def prediction():
 
         # print(summarise(text))
 
-app.run(host="localhost" , port=8080)
+# app.run(host="localhost" , port=8080)
+
+if __name__ == "__main__":
+        app.run(debug=True, host="0.0.0.0", port=8000)
